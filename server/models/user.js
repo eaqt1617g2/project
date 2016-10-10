47215bd60e5ae2708619534edc14bd166d6b7144
Jordi Lopez <jordi.lopez@estudiant.upc.edu>
2016-10-10 19:10:05 +0200

    Adaptar user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,13 @@
 var mongoose = require('mongoose');
 
 var User = mongoose.model('users', {
-	nombre: String,
-	apellidos: String,
-	email: String
+	name: String,
+	last_name: String,
+	loginid: String,
+	password: String,
+	creation_date: Date,
+	email: String,
+	last_login_date: Date
 });
 
 module.exports = User;
