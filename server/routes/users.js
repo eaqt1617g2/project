2e22c2a1271877663903d0a7bd2e02a42fbc8c37
Unknown <guillermo.riverola@outlook.com>
2016-10-10 19:25:46 +0200

    Put y server

--- server/routes/users.js
+++ server/routes/users.js
@@ -24,4 +24,53 @@ router.get('/:id', function(req, res,next) {
 
 });
 
+
+router.put('/api/users/:user', function(req, res) {
+    User.update({
+        id: req.params.user
+        //loginid: req.body.loginid
+        password: req.body.password,
+        name: req.body.name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+
+        done: false
+    }, function(err, user){
+        if(err) {
+            res.send(err);
+        }
+
+        User.find(function(err, users) {
+            if(err){
+                res.send(err);
+            }
+            res.json(users);
+        });
+    });
+});
+
+
+router.post('/api/users', function(req, res) {
+    User.create({
+        id: req.body.id
+        loginid: req.body.loginid
+        password: req.body.password,
+        name: req.body.name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        done: false
+    }, function(err, user){
+        if(err) {
+            res.send(err);
+        }
+
+        User.find(function(err, users) {
+            if(err){
+                res.send(err);
+            }
+            res.json(users);
+        });
+    });
+});
+
 module.exports = router;
