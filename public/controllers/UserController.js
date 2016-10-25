c39d855a9affe50c1ccad3b4cf7dbb0fa1a3d7a8
rosafernandez <kemalea@hotmail.com>
2016-10-25 19:20:03 +0200

    login/register centrado

--- public/controllers/UserController.js
+++ public/controllers/UserController.js
@@ -1,5 +1,20 @@
 userApp.controller("UserController", function(SERVER_INFO, $scope, $http, $location, $routeParams, $uibModal) {
-    
+    $(document).ready(function(){
+        var h = $("#header").height();
+        var t = $(document).height();
+        var lr = $("#loginregister").height();
+        var c = ((t-h)-lr)/2;
+        console.log(c);
+        $("#loginregister").css("padding-top", c);
+    });
+    $(document).resize(function() {
+        var h = $("#header").height();
+        var t = $(document).height();
+        var lr = $("#loginregister").height();
+        var c = ((t-h)-lr)/2;
+        console.log(c);
+        $("#loginregister").css("padding-top", c);
+    });
     var serverAddr = "http://"+SERVER_INFO.IP+":"+SERVER_INFO.PORT;
     console.log("Server: "+serverAddr);
     $scope.user= {};
